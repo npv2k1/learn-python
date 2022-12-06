1843c3175896af51da8d47ce2136ec3e11924791
Phạm Văn Nguyên <npv2k1@gmail.com>
2022-12-07 01:05:52 +0700

    example

---
+++ pythonbyexample/Bien/bien.py
@@ -0,0 +1,22 @@
+name = "Nguyen"
+print("Name:\t",name,type(name))
+age = 18
+print("Age:\t",age,type(age))
+gpa = 2.7
+print("GPA:\t",gpa,type(gpa))
+friend = ['Minh',"Linh"]
+print("Friend:\t",friend,type(friend))
+scores = {
+    "match":8.0,
+    "physics":9.0
+}
+print("Socres: ",scores,type(scores))
+love = ("unknown","any")
+print("Love:\t",love,type(love))
+colors = {"Blue", "Black", "Yellow"}
+print("Colors:\t",colors,type(colors))
+
+
+
+
+
---
+++ pythonbyexample/Hello world/hello-world.py
@@ -0,0 +1,2 @@
+from arepl_dump import dump
+print("Hello, Wordld!")
