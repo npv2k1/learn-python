cc601ecbacc479ed5df349bd40e415be8813927c
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-07-20 23:22:11 +0700

    Update windows

---
+++ windows/Tk/README.md
@@ -0,0 +1,16 @@
+# Tkinter
+Tkinter là một giao diện Python chuẩn đến bộ công cụ giao diện người dùng Tcl/Tk. Tkinter cho phép bạn tạo các ứng dụng GUI đa nền tảng.
+
+## Project
+
+### Random color
+
+Source: [random-color](random-color)
+
+![demo](random-color/demo.png)
+
+### Game tic tac toe
+
+Source: [game-xo](game-xo)
+
+![Alt text](game-xo/demo.png)
---
+++ windows/Tk/random-color/main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+import random
+root = Tk()
+
+def clamp(x):
+    return max(0, min(x, 255))
+
+def changeButtonColor():
+    colors = "#{0:02x}{1:02x}{2:02x}".format(
+        clamp(random.randint(0, 225)), clamp(random.randint(0, 225)), clamp(random.randint(0, 225)))
+    btn1['bg'] = colors
+    btn1['text'] = "Random Color" + '\n' + colors
+
+btn1 = Button(root, text="Random Color", padx=100,
+              pady=100, command=changeButtonColor)
+e = Entry(btn1, bg='black', width=50)
+btn1.pack()
+root.mainloop()
